06d7143ab4da6365b6e08b7c0157f46be3e0c191
Penjiboy <chavulapenjani@gmail.com>
2018-07-12 16:46:52 -0700

    Started working on the slider script

---
+++ PPFtest/Assets/Scripts/ScalePresets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalePresets : MonoBehaviour {
+
+    public GameObject RealSizeButton;
+    public GameObject WorkableSizeButton;
+    public GameObject RoomScaleButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
